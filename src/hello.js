7b258b0d37ca303c0e489ea645cc55cdd77afc2c
Nicolas Vahlas <nico@vahlas.eu>
2011-11-01 22:16:08 +0200

    first connection to mysql

--- src/hello.js
+++ src/hello.js
@@ -1,9 +1,21 @@
 
 var http = require('http');
- 
+var mysql = require('mysql');
+
 var server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Hello world\n: first joyent, node, github app :)");
+  var client = mysql.createClient({
+	"user":"nico",
+	"password":"m2gsL&D",
+	"database":"nodetests"
+  });
+
+  client.query('select * from person', function(err, results, fields) {
+	if (err) {
+		throw err;
+	}
+	res.writeHead(200, { "Content-Type": "text/plain" })
+	res.end("Hello world:\n first joyent, node, github app :)");
+  });
 });
  
 server.listen(process.env.PORT || 8001);
